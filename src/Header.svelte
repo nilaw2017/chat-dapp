0e62834c07583493127429df54dba013139e7771
nilaw2017 <nilaw.manandhar2016@gmail.com>
2022-01-28 19:30:26 +0545

    init

---
+++ src/Header.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { username, user } from "./user";
+
+  function signout() {
+    user.leave();
+    username.set("");
+  }
+</script>
+
+<header>
+  <img src="./../computer.svg.png" />
+  {#if $username}
+    <div class="user-bio">
+      <span>Hello <strong>{$username}</strong></span>
+      <img
+        src={`https://avatars.dicebear.com/api/initials/${$username}.svg`}
+        alt="avatar"
+      />
+    </div>
+
+    <button class="signout-button" on:click={signout}>Sign Out</button>
+  {:else}
+    <h3>BCA Chat</h3>
+  {/if}
+</header>
